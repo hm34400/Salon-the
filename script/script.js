0f05494ab9eebec90835f684a0ad544fe24179d5
hm34400 <hichamelyaagoubi34@gmail.com>
2016-08-27 19:13:08 +0200

    texte

---
+++ script/script.js
@@ -0,0 +1,24 @@
+//window.innerWidth
+//window.innerHeight
+//window.outerWidth
+//window.outerHeight
+
+$("header").css("height", window.innerHeight + 100 ); // Header Taille 
+//$(".grosse").css("height", window.innerHeight) ; // Header Taille
+
+
+
+$(document).ready(function(){ 
+          
+   $(window).scroll(function(){  /*Fonction ecoute le scroll*****/
+  var scrollTop = $(this).scrollTop();
+
+    if($(window).scrollTop() > 100){ /*Nombre de pixcel pour condition ok***/
+      
+          $('.navbar-default').addClass("bigcontent");/*Aplique la classe****/
+  }
+  else{$('.navbar-default').removeClass("bigcontent");}
+  
+       });
+       
+    });
